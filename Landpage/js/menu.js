53ac79a6ee510b5a929c303ed6e52d0a181ef0a0
Henry Buñay <Henry.bunay@gmail.com>
2021-06-20 09:11:21 +0200

    create menu animation with js

--- Landpage/js/menu.js
+++ Landpage/js/menu.js
@@ -4,3 +4,15 @@ const menu = document.querySelector(".menu-navegacion");
 /*creando efectos menu*/
 console.log(menu);
 console.log(hamburger);
+
+/*aparecer menu*/
+hamburger.addEventListener('click', ()=>{
+    menu.classList.toggle("spread");
+})
+window.addEventListener('click', e=>{
+    if (menu.classList.contains('spread')
+    && e.target != menu && e.target!= hamburger){
+        menu.classList.toggle('spread');
+        
+    }
+})
